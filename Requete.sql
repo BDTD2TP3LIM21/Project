1e3a96cc5de77dab6d3e921339bed6e0a23a0068
Firaifoxe <ac3.professionnel@gmail.com>
2021-05-03 22:33:58 +0200

    Update Requete.sql

--- Requete.sql
+++ Requete.sql
@@ -7,19 +7,54 @@ FROM DOCUMENTS
 WHERE Theme LIKE '%informatique%' OR Theme LIKE '%mathématiques%'
 GROUP BY TITLE;
 
+-- 2 --
+SELECT D.TITLE, D.THEME
+FROM DOCUMENTS D, BORROWERS BE, BORROWS BS
+WHERE BS.id_borrows = D.borrow_id
+AND BE.id_borrower = BS.borrower
+AND BS.begin_borrow > '15/11/2018'
+AND BS.end_borrow < '15/11/2019';
+
 -- 3 --
 
 SELECT documents.title, autors.surname_autors 
 FROM DOCUMENTS, AUTORS, BORROWS, BORROWERS
 WHERE borrows.id_borrows = documents.id_documents AND borrowers.id_borrower = borrows.borrower;
 
+-- 4 --
+
 -- 5 --
 
-SELECT COUNT (documents.id_documents) FROM DOCUMENTS, EDITORS
+SELECT COUNT (documents.id_documents) 
+FROM DOCUMENTS, EDITORS
 WHERE documents.editor_name = editors.name_editors AND name_editors = 'Eyrolles';
+
+-- 6 --
+
+SELECT editor_name, COUNT(*) as nb_doc
+FROM DOCUMENTS 
+group by editor_name;
 
 -- 7 --
 
-SELECT documents.title ,COUNT (documents.borrow_id)
-FROM DOCUMENTS;
+SELECT documents.id_documents, documents.title, COUNT (borrows.id_borrows) as nb_emprunt
+FROM DOCUMENTS LEFT JOIN BORROWS ON documents.borrow_id = borrows.id_borrows
+GROUP BY documents.id_documents, documents.title;
 
+-- 8 --
+
+SELECT editor_name 
+FROM DOCUMENTS
+WHERE Theme LIKE '%informatique' OR Theme LIKE '%mathématiques'
+group by editor_name
+HAVING COUNT(*) > 2;
+
+-- 9 --
+
+SELECT borrowers.name_borrower
+FROM BORROWERS
+WHERE borrowers.adresse IN (
+    SELECT borrowers.name_borrower
+    FROM BORROWERS
+    WHERE borrowers.name_borrower = 'Dupont'
+    );
