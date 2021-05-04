e4c054d9197dd6bc90eb4f15efac09c7311ef465
alexandre fournier <alexandre.fournier@etu.univ-poitiers.fr>
2021-05-05 00:58:16 +0200

    update crea / requete

--- Requete.sql
+++ Requete.sql
@@ -4,13 +4,13 @@
 
 SELECT TITLE 
 FROM DOCUMENTS
-WHERE Theme LIKE '%informatique%' OR Theme LIKE '%mathématiques%'
+WHERE Theme LIKE '%Informatique%' OR Theme LIKE '%Mathématiques%'
 GROUP BY TITLE;
 
 -- 2 --
 SELECT D.TITLE, D.THEME
 FROM DOCUMENTS D, BORROWERS BE, BORROWS BS
-WHERE BS.id_borrows = D.borrow_id
+WHERE BS.documents = D.id_documents
 AND BE.id_borrower = BS.borrower
 AND BS.begin_borrow > '15/11/2018'
 AND BS.end_borrow < '15/11/2019';
@@ -19,7 +19,7 @@ AND BS.end_borrow < '15/11/2019';
 
 SELECT documents.title, autors.surname_autors 
 FROM DOCUMENTS, AUTORS, BORROWS, BORROWERS
-WHERE borrows.id_borrows = documents.id_documents AND borrowers.id_borrower = borrows.borrower;
+WHERE borrows.documents = documents.id_documents AND borrowers.id_borrower = borrows.borrower;
 
 -- 4 --
 
@@ -27,27 +27,26 @@ WHERE borrows.id_borrows = documents.id_documents AND borrowers.id_borrower = bo
 
 SELECT COUNT (documents.id_documents) 
 FROM DOCUMENTS, EDITORS
-WHERE documents.editor_name = editors.name_editors AND name_editors = 'Eyrolles';
+WHERE documents.editor = editors.name_editors AND editors.name_editors = 'Eyrolles';
 
 -- 6 --
 
-SELECT editor_name, COUNT(*) as nb_doc
+SELECT editor, COUNT(*) as nb_doc
 FROM DOCUMENTS 
-group by editor_name;
+group by editor;
 
 -- 7 --
 
-SELECT documents.id_documents, documents.title, COUNT (borrows.id_borrows) as nb_emprunt
-FROM DOCUMENTS LEFT JOIN BORROWS ON documents.borrow_id = borrows.id_borrows
+SELECT documents.id_documents, documents.title, COUNT (borrows.documents) as nb_emprunt
+FROM DOCUMENTS LEFT JOIN BORROWS ON documents.id_documents = borrows.documents
 GROUP BY documents.id_documents, documents.title;
 
 -- 8 --
-
-SELECT editor_name 
-FROM DOCUMENTS
-WHERE Theme LIKE '%informatique' OR Theme LIKE '%mathématiques'
-group by editor_name
-HAVING COUNT(*) > 2;
+SELECT editor
+FROM DOCUMENTS 
+WHERE Theme LIKE '%Informatique%' OR Theme LIKE '%Mathématiques%'
+GROUP BY editor
+HAVING COUNT(editor) > 1;
 
 -- 9 --
 
@@ -64,11 +63,14 @@ WHERE borrowers.adresse IN (
 SELECT documents.
 
 /*10-*/
-SELECT editor
-FROM DOCUMENTS
-WHERE theme LIKE '%informatique'
-group by editor
-HAVING COUNT(*) < 1;
+SELECT D1.editor
+FROM DOCUMENTS D1
+WHERE D1.editor NOT IN (
+    SELECT D2.editor  
+    FROM DOCUMENTS D2
+    WHERE Theme LIKE '%Informatique%'
+    )
+GROUP BY editor;
 
 /*12-*/
 SELECT D1.title
@@ -76,14 +78,8 @@ FROM DOCUMENTS D1
 WHERE D1.id_documents NOT IN (
     SELECT D2.id_documents  
     FROM DOCUMENTS D2, BORROWS B
-    WHERE D2.id_doccuments = B.documents
+    WHERE D2.id_documents = B.documents
     );
     
 /*14-*/
-SELECT D1.title
-FROM DOCUMENTS D1
-WHERE D1.id_documents NOT IN (
-    SELECT D2.id_documents  
-    FROM DOCUMENTS D2, BORROWS B
-    WHERE D2.borrow_id = B.id_borrows
-    );
+
