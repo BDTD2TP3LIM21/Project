8f68b638c2a51ecaaff61391dfc0ba72f1e60d0b
alexandre fournier <alexandre.fournier@etu.univ-poitiers.fr>
2021-05-05 17:30:03 +0200

    ajout table + requete

--- Requete.sql
+++ Requete.sql
@@ -1,11 +1,11 @@
 ------ Partie sur les requêtes -----
 
 -- 1 --
-
 SELECT TITLE 
 FROM DOCUMENTS
 WHERE Theme LIKE '%Informatique%' OR Theme LIKE '%Mathématiques%'
-GROUP BY TITLE;
+GROUP BY TITLE
+ORDER BY title;
 
 -- 2 --
 SELECT D.TITLE, D.THEME
@@ -16,7 +16,6 @@ AND BS.begin_borrow > '15/11/2018'
 AND BS.end_borrow < '15/11/2019';
 
 -- 3 --
-
 SELECT documents.title, autors.surname_autors 
 FROM DOCUMENTS, AUTORS, BORROWS, BORROWERS
 WHERE borrows.documents = documents.id_documents AND borrowers.id_borrower = borrows.borrower;
@@ -24,19 +23,18 @@ WHERE borrows.documents = documents.id_documents AND borrowers.id_borrower = bor
 -- 4 --
 
 -- 5 --
-
-SELECT COUNT (documents.id_documents) 
-FROM DOCUMENTS, EDITORS
-WHERE documents.editor = editors.name_editors AND editors.name_editors = 'Eyrolles';
+SELECT editor, sum(quantity) as nb_Examplar
+FROM DOCUMENTS D, EDITORS E
+WHERE D.editor = E.name_editors 
+AND E.name_editors = 'Eyrolles'
+GROUP BY EDITOR;
 
 -- 6 --
-
 SELECT editor, COUNT(*) as nb_doc
 FROM DOCUMENTS 
 group by editor;
 
 -- 7 --
-
 SELECT documents.id_documents, documents.title, COUNT (borrows.documents) as nb_emprunt
 FROM DOCUMENTS LEFT JOIN BORROWS ON documents.id_documents = borrows.documents
 GROUP BY documents.id_documents, documents.title;
@@ -44,12 +42,11 @@ GROUP BY documents.id_documents, documents.title;
 -- 8 --
 SELECT editor
 FROM DOCUMENTS 
-WHERE Theme LIKE '%Informatique%' OR Theme LIKE '%Mathématiques%'
+WHERE theme LIKE '%Informatique%' OR Theme LIKE '%Mathématiques%'
 GROUP BY editor
 HAVING COUNT(editor) > 1;
 
 -- 9 --
-
 SELECT borrowers.name_borrower
 FROM BORROWERS
 WHERE borrowers.adresse IN (
@@ -57,10 +54,6 @@ WHERE borrowers.adresse IN (
     FROM BORROWERS
     WHERE borrowers.name_borrower = 'Dupont'
     );
-    
--- 11 --
-
-SELECT documents.
 
 /*10-*/
 SELECT D1.editor
@@ -68,12 +61,23 @@ FROM DOCUMENTS D1
 WHERE D1.editor NOT IN (
     SELECT D2.editor  
     FROM DOCUMENTS D2
-    WHERE Theme LIKE '%Informatique%'
+    WHERE D2.theme LIKE '%Informatique%'
     )
 GROUP BY editor;
 
+-- 11 --
+SELECT B1.name_borrower as never_borrowed
+FROM BORROWERS B1
+WHERE B1.name_borrower NOT IN (
+    SELECT B2.name_borrower
+    FROM BORROWERS B2, BORROWS BO
+    WHERE B2.name_borrower = BO.borrower
+    );
+
+
+
 /*12-*/
-SELECT D1.title
+SELECT D1.title as never_been_borrowed
 FROM DOCUMENTS D1
 WHERE D1.id_documents NOT IN (
     SELECT D2.id_documents  
@@ -81,5 +85,10 @@ WHERE D1.id_documents NOT IN (
     WHERE D2.id_documents = B.documents
     );
     
+/*13-*/
+SELECT B2.name_borrower, B2.surname_borrower
+FROM BORROWERS B2
+WHERE B2.name_categorie = 'Professionnel';
+
 /*14-*/
 
